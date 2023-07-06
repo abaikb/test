efff214c8fb3813e0d746a51ed1c4db50156408c
abaikb <abaikb@icloud.com>
2023-07-06 19:42:59 +0600

    add style

--- src/components/contribut/ContributionGraph.js
+++ src/components/contribut/ContributionGraph.js
@@ -47,7 +47,7 @@ const ContributionGraph = () => {
     );
 
     const currentDate = new Date();
-    const currentDateOffset = 50 * 7; 
+    const currentDateOffset = 50 * 7;
 
     return sortedContributions.map((day, index) => {
       const { date, contributions } = day;
@@ -84,14 +84,12 @@ const ContributionGraph = () => {
           onMouseEnter={() => handleContributionHover(true, index)}
           onMouseLeave={() => handleContributionHover(false, index)}
         >
-          {contributions > 0 && (
-            <span className="contribution-count"></span>
-          )}
+          {contributions > 0 && <span></span>}
           {isHovered[index] && (
             <div className="modal">
               <div className="modal-content">
-              <p>{contributions} contributions</p>
-                <p>{formatDate(date)}</p>
+                <p className="count">{contributions} contributions</p>
+                <p className="date">{formatDate(date)}</p>
               </div>
             </div>
           )}
@@ -150,6 +148,23 @@ const ContributionGraph = () => {
     <div className="contribution-graph">
       {renderContributions()}
       {renderModal()}
+      <div className="box_primary">
+        <div>Меньше</div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="84"
+          height="20"
+          viewBox="0 0 84 15"
+          fill="none"
+        >
+          <path d="M15.9054 0H0.904877V15H15.9054V0Z" fill="#EDEDED" />
+          <path d="M32.9059 0H17.9054V15H32.9059V0Z" fill="#ACD5F2" />
+          <path d="M49.9064 0H34.906V15H49.9064V0Z" fill="#7FA8C9" />
+          <path d="M66.907 0H51.9065V15H66.907V0Z" fill="#527BA0" />
+          <path d="M83.9075 0H68.907V15H83.9075V0Z" fill="#254E77" />
+        </svg>
+        <div>Больше</div>
+      </div>
     </div>
   );
 };
